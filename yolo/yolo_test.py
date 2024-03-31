742e12ef4a4abacddfa155b6052bf3c01f778bd2
parmimaher <101663511+parmimaher@users.noreply.github.com>
2024-03-31 21:12:34 +0300

    Delete yolo directory

--- yolo/yolo_test.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-model = YOLO('../yolo_weights/yolov8n.pt')
-results = model("baza_img/Getty_school_bus_safety_LARGE_Agnieszka-Kirinicjanow-56a13ea95f9b58b7d0bd5ff0.jpg", show = True)
-cv2.waitKey(0)
